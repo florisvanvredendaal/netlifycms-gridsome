d1bfba9733aed136a026dfec79059d24339b7685
F.M. van Vredendaal <floris@hatless.com>
2019-11-20 10:55:43 +0100

    Update Author.vue

--- src/components/Author.vue
+++ src/components/Author.vue
@@ -10,8 +10,7 @@
 		</p>
 
 		<p class="author__links">
-			<a href="//twitter.com/suits_at">Follow on Twitter</a>
-			<a href="https://github.com/suits-at/netlifycms-gridsome">GitHub</a>
+			<a target="blank" href="https://twitter.com/paypageHQ">Follow on Twitter 👉</a>
 		</p>
 
 	</div>
